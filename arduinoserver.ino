25465f62adf5eefabcd1a08563438db29b881a75
indrit11 <indmus12@htl-shkoder.com>
2018-02-06 18:19:14 +0100

    Add files via upload

---
+++ arduinoserver.ino
@@ -0,0 +1,33 @@
+#include <Ethernet.h>
+#include <SPI.h>
+
+IPAddress ip(192, 168, 115, 1);
+byte mac[] = {0xAA, 0xBB, 0xCC, 0xDD, 0xEE, 0xFF};
+
+EthernetServer server = EthernetServer(1234);
+
+void setup() {
+
+  Serial.begin(9600);
+
+  Ethernet.begin(mac, ip);
+
+  delay(1000);
+
+  server.begin();
+
+  Serial.println("Waiting for client...");
+
+}
+
+void loop() {
+
+  EthernetClient client = server.available();
+
+  if(client == true){
+
+    Serial.println("Connected");
+
+  }
+
+}
