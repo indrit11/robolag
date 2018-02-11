1ba106e5060ad4582e69a768efca435560e8b07f
indrit11 <indmus12@htl-shkoder.com>
2018-02-11 22:06:02 +0100

    Add files via upload

--- SerialArm.java
+++ SerialArm.java
@@ -19,8 +19,8 @@ import javax.swing.JLabel;
 import javax.swing.UIManager;
 
 import jssc.SerialPort;
-import jssc.SerialPortEvent;
-import jssc.SerialPortEventListener;
+import jssc.SerialPortEvent;            // Die Serial Arm macht auch die Seriell Port Verbindung mit Arduino.
+import jssc.SerialPortEventListener;    // Die Seriell Port Verbindung braucht die Importieren  von Seriell Port Packages.
 import jssc.SerialPortException;
 import jssc.SerialPortList;
 
@@ -45,7 +45,7 @@ public class SerialArm implements ActionListener, SerialPortEventListener
 
          Socket skt = new Socket("192.168.0.30", 1234);
 
-         BufferedReader in = new BufferedReader(new 					InputStreamReader(skt.getInputStream()));
+         BufferedReader in = new BufferedReader(new InputStreamReader(skt.getInputStream())); //Die TCP Socket Verbindung mit Arduino
 
          System.out.print("Received string: '");
 
@@ -71,8 +71,8 @@ public class SerialArm implements ActionListener, SerialPortEventListener
 		frame.setLayout(new FlowLayout());
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
-		base = new ButtonSet("Base", "Left", "Right", "<<", ">>", "o", "p");
-		shoulder = new ButtonSet("Shoulder", "Up", "Down", "<<", ">>", "q", "w");
+		base = new ButtonSet("Base", "Left", "Right", "<<", ">>", "o", "p");    //Die Buttons machen die Bewegung des Armes wie sie bewegen z.B links recht oben unten.                                                                           
+		shoulder = new ButtonSet("Shoulder", "Up", "Down", "<<", ">>", "q", "w"); //Und nach jede Bewegung es gibt eine Buchstaben als Output in Java.
 		elbow = new ButtonSet("Elbow", "Up", "Down", "<<", ">>", "e", "r");
 		wrist = new ButtonSet("Wrist", "CCW", "CC", "u", "i");
 		claw = new ButtonSet("Claw", "Open", "Close", "y", "t");
@@ -86,9 +86,10 @@ public class SerialArm implements ActionListener, SerialPortEventListener
 		sequencer.setVisible(true);
 		sequencer.pack();
 
-		String[] portNames = SerialPortList.getPortNames();
+		String[] portNames = SerialPortList.getPortNames(); //rufen wir die Port Name mit einem String
 		frame.setLocation(200, 50);
-		if (portNames.length == 0) 
+		if (portNames.length == 0) //ist eine IfAnweisung wo wir die Instanz des Seriellen Ports gefunden 
+                                          // wurde oder ob wir den Port finden oder nicht.
 		{
 			ready = false;
 		    label = new JLabel("Could not find any COM ports.");
@@ -107,6 +108,11 @@ public class SerialArm implements ActionListener, SerialPortEventListener
 		    {
 				@Override
 				public void actionPerformed(ActionEvent e)
+                    //Diese Methode wird aufgerufen, wenn ein ActionEvent an einer angemeldeten Komponente auftritt. 
+                    //Dort muss also das gewünschte Verhalten, was auf das Ereignis erfolgen soll, implementiert werden. 
+                    //Als Parameter wird der Methode eine Referenz auf das ActionEvent-Objekt übergeben, über welches 
+                   // weitere Informationen über das Ereignis ermittelt werden können.
+                    //Es findet die PortName und es macht eine ifAnweisung ob es richtig und ready ist.
 				{
 					String[] portNames = SerialPortList.getPortNames();
 					if(portNames.length != 0)
@@ -129,8 +135,8 @@ public class SerialArm implements ActionListener, SerialPortEventListener
 		for (int i = 0; i < portNames.length; i++){
 		    System.out.println(portNames[i]);
 		}
-		label = new JLabel("Select Arduino COM port: ");
-		frame.add(label);
+		label = new JLabel("Select Arduino COM port: "); //wird ein JComboBox erstellt die comSelect heißt und dann diese selektierten Port rufen.
+		frame.add(label);                               //Dann  werden die Port Parameters gesetzt und EventListener hinzugefügt.
 		comSelect = new JComboBox<>(portNames);
 		comSelect.setMaximumSize(new Dimension(70, 30));
 		frame.add(comSelect);
@@ -143,7 +149,9 @@ public class SerialArm implements ActionListener, SerialPortEventListener
 	}
 
 	@Override
-	public void actionPerformed(ActionEvent e) 
+	public void actionPerformed(ActionEvent e) //Propagates a SerialPortEvent event.Macht die Selektierung von COMPort 
+                                                     // und dann addiert die frames (base,elbow,wrist,claw,shoulder).
+                                                     // Hat ein Exception fur die SerialPort.	
 	{
 		if(e.getSource().equals(goButton))
 		{
@@ -172,7 +180,7 @@ public class SerialArm implements ActionListener, SerialPortEventListener
 					frame.pack();
 					frame.validate();
 				} catch (SerialPortException e1) {
-					// TODO Auto-generated catch block
+					
 					e1.printStackTrace();
 				}
 			}
@@ -189,12 +197,12 @@ public class SerialArm implements ActionListener, SerialPortEventListener
 	}
 
 	@Override
-	public void serialEvent(SerialPortEvent arg0) 
+	public void serialEvent(SerialPortEvent arg0) //SMacht ein Exception die liest den String von Arduino oder macht ein error.
 	{
 		try {
 			System.out.print(serial.readString(arg0.getEventValue()));
 		} catch (SerialPortException e) {
-			// TODO Auto-generated catch block
+			
 			e.printStackTrace();
 		}
               //"System.setProperty("gnu.io.rxtx.SerialPorts", "/dev/ttyACM0");"
